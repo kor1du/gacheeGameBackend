438cfeadbe9a375e67a1b25e567a1e460bf39a96
이떵춘 <kor1du0312@gmail.com>
2023-10-23 17:11:44 +0900

    initial commit

---
+++ src/main/java/com/gacheeGame/GacheeGameApplication.java
@@ -0,0 +1,13 @@
+package com.gacheeGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GacheeGameApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(GacheeGameApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gacheeGame/GacheeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gacheeGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GacheeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
